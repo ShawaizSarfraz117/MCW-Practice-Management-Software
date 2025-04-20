c3a2e00102da9c931892f4af87daa7739c787166
UsamAkram <Usamaakram17@gmail.com>
2025-04-20 12:23:31 +0400

    add settings page

---
+++ apps/back-office/src/app/(dashboard)/settings/page.tsx
@@ -0,0 +1,11 @@
+import TopBar from "@/components/layouts/Topbar";
+export default function SettingsPage() {
+  return (
+    <>
+      <TopBar />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">Settings</h1>
+      </div>
+    </>
+  );
+}
