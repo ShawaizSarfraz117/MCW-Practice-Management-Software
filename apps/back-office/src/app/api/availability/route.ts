1b70c20280bff64d1773f83456360980ec62b36a
maazashraf <itsmaazashraf@gmail.com>
2025-05-01 11:02:19 +0500

    fix availability display

--- apps/back-office/src/app/api/availability/route.ts
+++ apps/back-office/src/app/api/availability/route.ts
@@ -70,7 +70,6 @@ export async function POST(request: NextRequest) {
 export async function GET(request: NextRequest) {
   try {
     const session = await getServerSession(backofficeAuthOptions);
-    console.log("request>>>", request);
     if (!session?.user) {
       console.log("getServerSession session>>>", session);
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
