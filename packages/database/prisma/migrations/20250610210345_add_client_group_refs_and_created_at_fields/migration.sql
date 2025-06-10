b3926cf9ce265330a84fa9aba6ee1005e542d7eb
Muzammil Shabbir <muzammil.techliance.com>
2025-06-11 02:07:33 +0500

    add created_at field in models for filtering data in overview tab

--- packages/database/prisma/migrations/20250610210345_add_client_group_refs_and_created_at_fields/migration.sql
+++ packages/database/prisma/migrations/20250610210345_add_client_group_refs_and_created_at_fields/migration.sql
@@ -3,13 +3,21 @@ BEGIN TRY
 BEGIN TRAN;
 
 -- AlterTable
+ALTER TABLE [dbo].[Appointment] ADD [created_at] DATETIME NOT NULL CONSTRAINT [Appointment_created_at_df] DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE [dbo].[ClientGroupChartNote] ADD [created_at] DATETIME NOT NULL CONSTRAINT [ClientGroupChartNote_created_at_df] DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
 ALTER TABLE [dbo].[DiagnosisTreatmentPlan] ADD [client_group_id] UNIQUEIDENTIFIER;
 
 -- AlterTable
-ALTER TABLE [dbo].[GoodFaithEstimate] ADD [client_group_id] UNIQUEIDENTIFIER;
+ALTER TABLE [dbo].[GoodFaithEstimate] ADD [client_group_id] UNIQUEIDENTIFIER,
+[created_at] DATETIME NOT NULL CONSTRAINT [GoodFaithEstimate_created_at_df] DEFAULT CURRENT_TIMESTAMP;
 
 -- AlterTable
-ALTER TABLE [dbo].[SurveyAnswers] ADD [client_group_id] UNIQUEIDENTIFIER;
+ALTER TABLE [dbo].[SurveyAnswers] ADD [client_group_id] UNIQUEIDENTIFIER,
+[created_at] DATETIME NOT NULL CONSTRAINT [SurveyAnswers_created_at_df] DEFAULT CURRENT_TIMESTAMP;
 
 -- CreateIndex
 CREATE NONCLUSTERED INDEX [IX_DiagnosisTreatmentPlan_ClientGroupId] ON [dbo].[DiagnosisTreatmentPlan]([client_group_id]);
